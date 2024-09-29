9822dc1d6dc9a4b39278d6a14abf8accc9c8af7a
Modi08 <indiabestgo@gmail.com>
2024-09-29 15:45:07 +0200

    Add user authentication

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,7 +1,19 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+//import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:recyclingapp/components/button.dart';
+import 'package:recyclingapp/components/mytextfield.dart';
+import 'package:http/http.dart' as http;
+import 'package:recyclingapp/services/general/snackBar.dart';
+import 'package:recyclingapp/signup_screen.dart';
+import 'package:recyclingapp/welcome_screen.dart';
+
+/*
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
+  final emailController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -21,14 +33,14 @@ class LoginScreen extends StatelessWidget {
                 style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
               ),
               const SizedBox(height: 20),
-              TextField(
+              const TextField(
                 decoration: InputDecoration(
                   labelText: 'Email',
                   border: OutlineInputBorder(),
                 ),
               ),
               const SizedBox(height: 20),
-              TextField(
+              const TextField(
                 decoration: InputDecoration(
                   labelText: 'Password',
                   border: OutlineInputBorder(),
@@ -48,6 +60,106 @@ class LoginScreen extends StatelessWidget {
                 child: const Text('Log In'),
               ),
             ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+*/
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final emailController = TextEditingController();
+  final pwdController = TextEditingController();
+
+  void login() async {
+    //await dotenv.load();
+    String apiUrl = "https://qeh35ldygc.execute-api.eu-central-1.amazonaws.com";
+
+    var paramsApiUrl =
+        "$apiUrl/recyclingLoginApp?email=${emailController.text}&pwd=${pwdController.text}";
+
+    var response = http.put(Uri.parse(paramsApiUrl));
+    response.then((http.Response response) {
+      print(response.body);
+      showSnackbar(context, jsonDecode(response.body)['msg'],
+          response.statusCode == 400);
+
+      if (response.statusCode == 200) {
+        Navigator.pushReplacement(context,
+            MaterialPageRoute(builder: (context) => const WelcomeScreen()));
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return OverflowBox(
+      child: Scaffold(
+        appBar: AppBar(
+            title: const Text("Login"),
+            backgroundColor: const Color.fromARGB(255, 222, 255, 222)),
+        body: SafeArea(
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const SizedBox(height: 50),
+                    Icon(
+                      Icons.recycling_sharp,
+                      size: 80,
+                      color: Colors.grey[800],
+                    ),
+                    const SizedBox(height: 50),
+                    const Text(
+                      "Welcome back. You' ve been missed!",
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    const SizedBox(height: 25),
+                    MyTextField(controller: emailController, hintText: "Email"),
+                    const SizedBox(height: 10),
+                    MyTextField(
+                        controller: pwdController,
+                        hintText: "Password",
+                        obscureText: true),
+                    const SizedBox(
+                      height: 25,
+                    ),
+                    CButton(onTap: login, text: "Login"),
+                    const SizedBox(
+                      height: 50,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text("Don't have an account?"),
+                        const SizedBox(width: 4),
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.pushReplacement(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) =>
+                                        const SignUpScreen()));
+                          },
+                          child: const Text(
+                            'Register now',
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                        )
+                      ],
+                    ),
+                  ]),
+            ),
           ),
         ),
       ),
--- lib/signup_screen.dart
+++ lib/signup_screen.dart
@@ -1,5 +1,12 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:recyclingapp/components/button.dart';
+import 'package:recyclingapp/components/mytextfield.dart';
+import 'package:recyclingapp/services/general/snackbar.dart';
+import 'package:recyclingapp/welcome_screen.dart';
+/*
 class SignUpScreen extends StatelessWidget {
   const SignUpScreen({super.key});
 
@@ -21,14 +28,14 @@ class SignUpScreen extends StatelessWidget {
                 style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
               ),
               const SizedBox(height: 20),
-              TextField(
+              const TextField(
                 decoration: InputDecoration(
                   labelText: 'Email',
                   border: OutlineInputBorder(),
                 ),
               ),
               const SizedBox(height: 20),
-              TextField(
+              const TextField(
                 decoration: InputDecoration(
                   labelText: 'Password',
                   border: OutlineInputBorder(),
@@ -52,5 +59,121 @@ class SignUpScreen extends StatelessWidget {
         ),
       ),
     );
+  }
+}
+*/
+
+class SignUpScreen extends StatefulWidget {
+  const SignUpScreen({super.key});
+
+  @override
+  State<SignUpScreen> createState() => _SignUpScreenState();
+}
+
+class _SignUpScreenState extends State<SignUpScreen> {
+  final emailController = TextEditingController();
+  final pwdController = TextEditingController();
+  final confirmPwdController = TextEditingController();
+  final usernameController = TextEditingController();
+
+  void signUp() async {
+    //await dotenv.load();
+    String apiUrl = "https://qeh35ldygc.execute-api.eu-central-1.amazonaws.com";
+
+    if (pwdController.text != confirmPwdController.text) {
+      showSnackbar(context, "Passwords don't match", true);
+      return null;
+    }
+
+    var paramsApiUrl =
+        "$apiUrl/recyclingSignUp?email=${emailController.text}&pwd=${pwdController.text}&username=${usernameController.text}";
+
+    var response = http.post(Uri.parse(paramsApiUrl));
+    response.then((http.Response response) {
+      showSnackbar(context, jsonDecode(response.body)['msg'],
+          response.statusCode == 400);
+
+      if (response.statusCode == 200) {
+        //saveDataToLocalStorage("userId", jsonDecode(response.body)['userId']);
+        //saveDataToLocalStorage("username", usernameController.text);
+
+        Navigator.pushReplacement(context,
+            MaterialPageRoute(builder: (context) => const WelcomeScreen()));
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return OverflowBox(
+        child: Scaffold(
+      appBar: AppBar(
+        title: const Text('Sign Up'),
+        backgroundColor: Colors.green,
+      ),
+      backgroundColor: Colors.grey[300],
+      body: SafeArea(
+          child: SingleChildScrollView(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25.0),
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+              const SizedBox(height: 50),
+              Icon(
+                Icons.message,
+                size: 80,
+                color: Colors.grey[800],
+              ),
+              const SizedBox(height: 50),
+              const Text(
+                "Let's create your account",
+                style: TextStyle(fontSize: 16),
+              ),
+              const SizedBox(height: 25),
+              MyTextField(controller: usernameController, hintText: "Username"),
+              const SizedBox(height: 10),
+              MyTextField(controller: emailController, hintText: "Email"),
+              const SizedBox(height: 10),
+              MyTextField(
+                  controller: pwdController,
+                  hintText: "Password",
+                  obscureText: true),
+              const SizedBox(height: 10),
+              MyTextField(
+                  controller: confirmPwdController,
+                  hintText: "Confirm Password",
+                  obscureText: true),
+              const SizedBox(
+                height: 25,
+              ),
+              CButton(onTap: signUp, text: "Sign up"),
+              const SizedBox(
+                height: 50,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text("Already have an account?"),
+                  const SizedBox(width: 4),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const SignUpScreen()));
+                    },
+                    child: const Text(
+                      'Login now',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  )
+                ],
+              ),
+            ]),
+          ),
+        ),
+      )),
+    ));
   }
 }
