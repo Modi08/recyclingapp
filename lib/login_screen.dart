1730e160688d17f7db4d24b4bdd6febf764d4305
Modi08 <indiabestgo@gmail.com>
2024-10-12 16:12:18 +0200

    Added a profile page

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,72 +1,14 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-//import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:recyclingapp/components/button.dart';
 import 'package:recyclingapp/components/mytextfield.dart';
 import 'package:http/http.dart' as http;
+import 'package:recyclingapp/profile_screen.dart';
+import 'package:recyclingapp/services/general/colors.dart';
 import 'package:recyclingapp/services/general/snackBar.dart';
 import 'package:recyclingapp/signup_screen.dart';
 import 'package:recyclingapp/welcome_screen.dart';
-
-/*
-class LoginScreen extends StatelessWidget {
-  const LoginScreen({super.key});
-  final emailController = TextEditingController();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Log In'),
-        backgroundColor: const Color.fromARGB(255, 205, 254, 206),
-      ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(
-                'Log In Page',
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-              ),
-              const SizedBox(height: 20),
-              const TextField(
-                decoration: InputDecoration(
-                  labelText: 'Email',
-                  border: OutlineInputBorder(),
-                ),
-              ),
-              const SizedBox(height: 20),
-              const TextField(
-                decoration: InputDecoration(
-                  labelText: 'Password',
-                  border: OutlineInputBorder(),
-                ),
-                obscureText: true,
-              ),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  // Add your log-in logic here
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.green,
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 40, vertical: 15),
-                ),
-                child: const Text('Log In'),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-*/
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -94,19 +36,20 @@ class _LoginScreenState extends State<LoginScreen> {
 
       if (response.statusCode == 200) {
         Navigator.pushReplacement(context,
-            MaterialPageRoute(builder: (context) => const WelcomeScreen()));
+            MaterialPageRoute(builder: (context) => const ProfileScreen()));
       }
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return OverflowBox(
-      child: Scaffold(
-        appBar: AppBar(
-            title: const Text("Login"),
-            backgroundColor: const Color.fromARGB(255, 222, 255, 222)),
-        body: SafeArea(
+    return Scaffold(
+      appBar: AppBar(
+          title: const Text("Login"),
+          backgroundColor: appBarColor),
+      backgroundColor: backgroundColor,
+      body: SingleChildScrollView(
+        child: SafeArea(
           child: Center(
             child: Padding(
               padding: const EdgeInsets.symmetric(horizontal: 25.0),
@@ -117,7 +60,7 @@ class _LoginScreenState extends State<LoginScreen> {
                     Icon(
                       Icons.recycling_sharp,
                       size: 80,
-                      color: Colors.grey[800],
+                      color: logoColor,
                     ),
                     const SizedBox(height: 50),
                     const Text(
---
+++ lib/services/general/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const appBarColor = Color.fromARGB(255, 222, 255, 222);
+var logoColor = Colors.grey[800];
+var backgroundColor = Colors.grey[300];
+var textColor = Colors.black;
